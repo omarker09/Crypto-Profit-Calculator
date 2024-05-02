0627ac7df49c8dbabe1cb0390a323491f0cf3836
Omar_09 <brigades.anony99@gmail.com>
2024-05-02 16:08:14 +0100

    CoinGeko API

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,16 +2,25 @@
 import React, { useState, useEffect } from "react";
 import "../app/globals.css"
 
+// Icons
 import { FaBitcoin } from "react-icons/fa6";
 import { CiSearch } from "react-icons/ci";
 import { MdOutlineArrowDropUp } from "react-icons/md";
 import { MdOutlineArrowDropDown } from "react-icons/md";
 import { FaRegTrashCan } from "react-icons/fa6";
+import { MdDoneOutline } from "react-icons/md";
+import { MdError } from "react-icons/md";
 
-const MainApp = () => {
+// External Components
+import { toast } from "sonner"
+import Stack from '@mui/material/Stack';
+import LinearProgress from '@mui/material/LinearProgress';
+import TooltipCustom from "./components/tooltip";
+
+const MainApp = ({ setIsLoading }: any) => {
 
   // State
-  const [search, setSearch] = useState<string>("");
+  const [searchQuery, setSearchQuery] = useState<string>("");
   const [InitialValue, setInitialValue] = useState<any>();
   const [cryptoPrice, setCryptoPrice] = useState<any>();
   const [sellPrice, setSellPrice] = useState<any>()
@@ -21,25 +30,38 @@ const MainApp = () => {
   const [totalProfit, setTotalProfit] = useState<any>();
   const [profitPercentage, setProfitPercentage] = useState<any>();
   const [isGain, setIsGain] = useState<boolean>(true)
+  const [data, setData] = useState<any>()
 
   // Calculations
   function Calculate() {
-    let amount = (InitialValue || 0) / (cryptoPrice || 0);
+    let amount = (InitialValue || 0) / ((cryptoPrice && data) || 0);
     let output = (amount * (sellPrice || 0)) * (1 - (investFeePercentage || 0) / 100) * (1 - (exitFeePercentage || 0) / 100);
     let profit = output - (InitialValue || 0);
-    let profitPercentage = (profit / (InitialValue || 0)) * 100;
+    let profitPercentage = (profit / (InitialValue || 0)) * 100
+
+    if (!cryptoPrice || !sellPrice) {
+      setMaxoutput("");
+      setProfitPercentage("");
+      setTotalProfit("")
+      setIsGain(true)
+      return;
+    }
     setProfitPercentage(profitPercentage);
     setTotalProfit(profit);
     setMaxoutput(output);
+
     if (output < (InitialValue || 0)) {
       setIsGain(false)
     } else if (output > (InitialValue || 0)) {
       setIsGain(true)
+    } else {
+      setIsGain(false)
     }
   }
 
+  // Clear User Inputs
   function ClearInputs() {
-    setSearch("");
+    setSearchQuery("");
     setCryptoPrice("");
     setExitFeePercentage("");
     setInitialValue("");
@@ -47,13 +69,69 @@ const MainApp = () => {
     setIsGain(true)
     setMaxoutput("");
     setProfitPercentage("");
-    setSellPrice("")
     setTotalProfit("")
+    setSellPrice("")
+    setData("")
   }
 
+  // Execute Calculate Function While The Input Value Change
   useEffect(() => {
     Calculate();
   }, [InitialValue, cryptoPrice, sellPrice, investFeePercentage, exitFeePercentage])
+
+  // Fetching The Buy Price From CoinGeko API
+  function fetchData() {
+    const url = `https://api.coingecko.com/api/v3/simple/price?ids=${searchQuery}&vs_currencies=usd&supported_vs_currencies`;
+    const options = {
+      method: 'GET',
+      headers: { accept: 'application/json', 'x-cg-demo-api-key': 'CG-2nAunhg98kHZ1zBYTPGRNx9j' }
+    };
+    // If The Search Input Is Empty then return nothing
+    if (!searchQuery) {
+      toast("Search Input Is Empty", {
+        icon: <MdError className=" text-red-600" size={20} />,
+        duration: 2500
+      })
+      return;
+    } else {
+      setIsLoading(true)
+      fetch(url, options)
+        .then(res => res.json())
+        .then(datas => {
+          const response = JSON.stringify(datas); // Assuming data is your JSON object
+          // Define a regular expression to match the "usd" value
+          const regex = /"usd":(\d+(\.\d+)?)/;
+
+          // Use match to extract the usd value
+          const match = response.match(regex);
+
+          // If match is found, extract the value, otherwise, set it to undefined
+          const usdValue = match ? Number(match[1]) : undefined;
+          if (!usdValue) {
+            setIsLoading(false)
+            toast("Wrong Coin Name try another name", {
+              icon: <MdError className=" text-red-600" size={20} />,
+              duration: 2500
+            })
+          } else if (usdValue) {
+            setIsLoading(false)
+            setData(usdValue);
+            setCryptoPrice(usdValue);
+            toast("Coin Price Has been Added", {
+              icon: <MdDoneOutline className=" text-green-500" size={15} />,
+              duration: 2500
+            })
+          }
+        })
+        .catch(err => {
+          setIsLoading(false)
+          toast("Someting Went Wrong try later", {
+            icon: <MdError className=" text-red-600" size={20} />,
+            duration: 2500
+          })
+        });
+    }
+  }
 
   return (
     <div className=" w-full sm:w-[700px] flex flex-col py-4 px-5 gap-5 items-center justify-center">
@@ -63,10 +141,13 @@ const MainApp = () => {
       </span>
       <div className="grid grid-cols-1 sm:grid-cols-2 gap-3 w-full">
         <div className="flex h-24 flex-col gap-1 w-full">
-          <p>Coin</p>
+          <div className="flex items-center justify-start gap-2">
+            <p>Coin Name</p>
+            <TooltipCustom title="You must Search only By Name not (BTC,ETH) only full name" />
+          </div>
           <div className="flex gap-1  items-center w-full justify-between py-3 px-3 rounded-lg custom-border">
-            <input value={search} onChange={(e) => { setSearch(e.target.value) }} className="custom-focus w-full" type="text" placeholder="Search Coin" />
-            <span className="duration-300 bg-gray-200 sm:bg-transparent hover:bg-gray-200 rounded-full p-2 active:bg-gray-300">
+            <input value={searchQuery} onChange={(e) => { setSearchQuery(e.target.value) }} className="custom-focus w-full" type="text" placeholder="Search Coin (Bitcoin,Ethereum).." />
+            <span onClick={() => { fetchData() }} className="duration-300 bg-gray-200 sm:bg-transparent hover:bg-gray-200 rounded-full p-2 active:bg-gray-300">
               <CiSearch size={25} className=" duration-300  rounded-full cursor-pointer" />
             </span>
           </div>
@@ -92,9 +173,10 @@ const MainApp = () => {
               $
             </span>
             <div className=" px-2 flex items-center w-full justify-between gap-1">
-              <input value={cryptoPrice} onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+              <input value={data && cryptoPrice} onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
                 const newValue = parseFloat(e.target.value)
                 setCryptoPrice(newValue);
+                setData(newValue)
               }} className="custom-focus w-full py-4" type="number" placeholder="0" />
             </div>
           </div>
@@ -120,7 +202,7 @@ const MainApp = () => {
               %
             </span>
             <div className="px-2 flex items-center w-full justify-between gap-1">
-              <input value={exitFeePercentage} onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+              <input value={exitFeePercentage} minLength={1} maxLength={100} onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
                 const newValue = parseFloat(e.target.value)
                 setExitFeePercentage(newValue);
               }} className="custom-focus w-full py-4" type="number" placeholder="0" />
@@ -134,7 +216,7 @@ const MainApp = () => {
               %
             </span>
             <div className=" px-2 flex items-center w-full justify-between gap-1">
-              <input value={investFeePercentage} onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+              <input value={investFeePercentage}  onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
                 const newValue = parseFloat(e.target.value)
                 setInvestFeePercentage(newValue);
               }} className="custom-focus w-full py-4" type="number" placeholder="0" />
@@ -142,10 +224,10 @@ const MainApp = () => {
           </div>
         </div>
       </div>
-      <div className=" w-full h-auto custom-border rounded-lg p-3">
+      <div className=" w-full h-auto custom-border rounded-lg p-3 overflow-hidden">
         <div className="flex items-center justify-between">
           <h1 className="text-base sm:text-2xl font-bold">Investment Resault :</h1>
-          <div onClick={() => {ClearInputs()}} className="flex items-center gap-1 cursor-pointer bg-orange-400 p-1 sm:p-2 rounded-lg">
+          <div onClick={() => { ClearInputs() }} className="flex items-center gap-1 cursor-pointer bg-orange-400 p-1 sm:p-2 rounded-lg">
             <FaRegTrashCan className="text-white" />
             <p className="text-white text-sm sm:text-base">Clear</p>
           </div>
@@ -155,21 +237,21 @@ const MainApp = () => {
             <p className=" text-gray-500">Profit/Loss</p>
             <div className={isGain !== true ? "p-2 bg-red-200 w-min text-center rounded-lg flex items-center" : "p-2 bg-green-200 w-min text-center rounded-lg flex items-center"}>
               {isGain !== true ? <MdOutlineArrowDropDown className=" text-red-700 font-bold" /> : <MdOutlineArrowDropUp className=" text-green-700 font-bold" />}
-              <span className="text-green-500 font-bold"> {(Math.abs((totalProfit || 0)).toFixed(2)) || 0}$</span>
-              <span className="text-red-500 font-bold"> ({((profitPercentage || 0).toFixed(2)) || 0}%)</span>
+              <span className="text-green-500 font-bold">{(Math.abs((totalProfit || 0)).toFixed(2)) || 0}$</span>
+              <span className="text-red-500 font-bold">({((profitPercentage || 0).toFixed(2)) || 0}%)</span>
             </div>
           </div>
           <div className=" flex items-center justify-start w-full gap-7">
             <div className="flex flex-col gap-1">
               <p className=" text-gray-500">Investment Amount</p>
               <div className="p-2 bg-green-200 w-min text-center rounded-lg flex items-center">
-                <span className="text-green-500 font-bold"> {(InitialValue || 0).toLocaleString()}$</span>
+                <span className="text-green-500 font-bold">{(InitialValue || 0).toLocaleString()}$</span>
               </div>
             </div>
             <div className="flex flex-col gap-1">
               <h1 className=" text-gray-500">Total</h1>
               <div className="p-2 bg-green-200 w-min text-center rounded-lg flex items-center">
-                <span className="text-green-500 font-bold"> {(maxOutput || 0).toLocaleString()}$</span>
+                <span className="text-green-500 font-bold">{(maxOutput || 0).toLocaleString()}$</span>
               </div>
             </div>
           </div>
@@ -180,9 +262,14 @@ const MainApp = () => {
 }
 
 export default function Home() {
+  // Checking Progress
+  const [isLoading, setIsLoading] = useState<boolean>()
   return (
     <div className="w-full flex flex-col justify-center items-center">
-      <MainApp />
+      {isLoading && <Stack sx={{ width: '100%', color: 'grey.500' }} spacing={2}>
+        <LinearProgress color="secondary" />
+      </Stack>}
+      <MainApp setIsLoading={setIsLoading} isLoading={isLoading} />
     </div>
   );
 }
